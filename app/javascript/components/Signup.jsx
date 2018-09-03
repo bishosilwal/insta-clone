3eb962205400d01d606e106a62df1c21a3c0a520
bishosilwal <aayussilwal@gmail.com>
2018-09-03 15:51:40 +0545

    axios package installed

--- app/javascript/components/Signup.jsx
+++ app/javascript/components/Signup.jsx
@@ -29,31 +29,31 @@ export default class Signup extends Component{
           <div className="col-4 text-center mt-5">
             <h3>Please Sign up </h3>
             <form onSubmit={this.handleSubmit}>
-              <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-2 col-form-label">Email:</label>
-                <div class="col-sm-10">
-                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email" name="email" autofocus onChange={this.handleInput} required/>
+              <div className="form-group row">
+                <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email:</label>
+                <div className="col-sm-10">
+                  <input type="email" className="form-control" id="inputEmail3" placeholder="Email" name="email" autoFocus onChange={this.handleInput} required/>
                 </div>
               </div>
-              <div class="form-group row">
-                <label for="inputPassword3" class="col-sm-2 col-form-label">Password:</label>
-                <div class="col-sm-10">
-                  <input type="password" class="form-control" id="inputPassword3" placeholder="Password" name="password" onChange={this.handleInput} required/>
+              <div className="form-group row">
+                <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password:</label>
+                <div className="col-sm-10">
+                  <input type="password" className="form-control" id="inputPassword3" placeholder="Password" name="password" onChange={this.handleInput} required/>
                 </div>
               </div>
-              <div class="form-group row">
-                <label for="username" class="col-sm-2 col-form-label">Username:</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="username" placeholder="username" name="username" onChange={this.handleInput}/>
+              <div className="form-group row">
+                <label htmlFor="username" className="col-sm-2 col-form-label">Username:</label>
+                <div className="col-sm-10">
+                  <input type="text" className="form-control" id="username" placeholder="username" name="username" onChange={this.handleInput}/>
                 </div>
               </div>
-              <div class="form-group row">
-                <div class="col-sm-10">
-                  <button type="submit" class="btn btn-primary">Sign up</button>
+              <div className="form-group row">
+                <div className="col-sm-10">
+                  <button type="submit" className="btn btn-primary">Sign up</button>
                 </div>
               </div>
-              <div class="form-group row">
-                <div class="col-sm-10">
+              <div className="form-group row">
+                <div className="col-sm-10">
                   <Link to="/login">Login</Link>
                 </div>
               </div>
