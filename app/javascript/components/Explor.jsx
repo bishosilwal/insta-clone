7149626e20d7c74dc750e5cec05a13c2c932c1a2
bishosilwal <aayussilwal@gmail.com>
2018-09-06 11:53:54 +0545

    all suggested user all listed in explor component

--- app/javascript/components/Explor.jsx
+++ app/javascript/components/Explor.jsx
@@ -6,16 +6,17 @@ const HEADERS =  JSON.parse(window.localStorage.getItem('headers'))
 export default class Explor extends Component{
 
   state={
-    
+    allUser: []
   }
 
   componentDidMount(){
+    var self = this
     axios.get(HOST+'friends/suggestion',
     {
       headers: HEADERS
     })
     .then(function (res){
-      console.log(res)
+      self.setState({allUser: res.data})
     })
     .catch(function (err){
       console.log(err)
@@ -23,14 +24,28 @@ export default class Explor extends Component{
   }
 
   render(){
+    console.log(this.state.allUser)
+    const { allUser } = this.state
     return(
         <div className="container-fluid ">
           <div className="row justify-content-center">
             <div className="col-10 text-left">
               Discover People
             </div>
-            <div className="col-10 ">
-              peoples
+            <div className="col-10 row">
+              {
+                allUser.map(function(user, index){
+                  return(
+                      <div className="card m-2 user-suggest-card shadow" key={index}>
+                        <div className="card-body">
+                          <h5 className="card-title">{user.name}</h5>
+                          <label> { user.email } </label><br/>  
+                          <a href="#" className="btn btn-primary btn-sm">Follow</a>
+                        </div>
+                      </div>
+                    )
+                })
+              }
             </div>
           </div>
         </div>
