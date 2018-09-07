b5c2ba30b3b698abd46aa721d4c350fea5e82068
bishosilwal <aayussilwal@gmail.com>
2018-09-07 11:43:09 +0545

    notification create action added after friendship created

--- app/models/friend_ship.rb
+++ app/models/friend_ship.rb
@@ -1,4 +1,9 @@
 class FriendShip < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+  after_create_commit :create_notification
+
+  def create_notification
+    Notification.create(user_id: self.user_id, receiver_id: self.friend_id, message: 'followed you')
+  end
 end
